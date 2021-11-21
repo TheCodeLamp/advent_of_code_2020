67691805368d43ee7089829f30dfe5d44ce56236
Simon Pertoft <simon@spectral.ninja>
2021-11-21 16:09:41 +0100

    part 2 working

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use regex::Regex;
 
 use crate::utils;
 
@@ -76,15 +77,79 @@ impl Passport {
 }
 
 fn is_valid(pass: &Passport) -> bool {
-    if pass.missing_keys.len() > 0 {
-        if pass.missing_keys.len() == 1 && pass.missing_keys.contains(&"cid".to_string()) {
-            return true;
+    let mut checks = 0;
+
+    for (key, val) in pass.data_map.clone() {
+        if key.eq("byr") { // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+            let parse_val: usize = match val.parse::<usize>() {
+                Ok(res) => res,
+                Err(_) => continue,
+            };
+            if parse_val >= 1920 && parse_val <= 2002 {
+                checks += 1;
+                continue;
+            }
         }
-    } else {
-        return true;
+        if key.eq("iyr") { // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+            let parse_val: usize = match val.parse::<usize>() {
+                Ok(res) => res,
+                Err(_) => continue,
+            };
+            if parse_val >= 2010 && parse_val <= 2020 {
+                checks += 1;
+                continue;
+            }
+        }
+        if key.eq("eyr") { // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+            let parse_val: usize = match val.parse::<usize>() {
+                Ok(res) => res,
+                Err(_) => continue,
+            };
+            if parse_val >= 2020 && parse_val <= 2030 {
+                checks += 1;
+                continue;
+            }
+        }
+        if key.eq("hgt") { // hgt (Height) - a number followed by either cm or in: 
+            if val.contains("cm") { // If cm, the number must be at least 150 and at most 193.
+                let parse_val: usize = val.replace("cm", "").parse::<usize>().unwrap();
+                if parse_val >= 150 && parse_val <= 193 {
+                    checks += 1;
+                    continue;
+                }
+            }
+            if val.contains("in") { // If in, the number must be at least 59 and at most 76.
+                let parse_val: usize = val.replace("in", "").parse::<usize>().unwrap();
+                if parse_val >= 59 && parse_val <= 76 {
+                    checks += 1;
+                    continue;
+                }
+            }
+        }
+        if key.eq("hcl") { // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            let re = Regex::new(r"^\#[0-9a-f]{6}$").unwrap();
+            if re.is_match(val.as_str()){
+                checks += 1;
+                continue;
+            }
+        }
+        if key.eq("ecl") { // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+            let valid_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+            if valid_colors.contains(&val.as_str()){
+                checks += 1;
+                continue;         
+            }
+        }
+        if key.eq("pid") { // pid (Passport ID) - a nine-digit number, including leading zeroes.
+            let re = Regex::new(r"^\d{9}$").unwrap();
+            if re.is_match(val.as_str()) {
+                checks += 1;
+                continue;
+            }
+        }
     }
 
-    return false;
+    return checks == 7;
 }
 
 #[allow(dead_code)]
@@ -97,7 +162,7 @@ pub fn run_part_1() -> Result<usize, String> {
 
     for line in raw_input {
         let tokenized_line: Vec<&str> = line.split(" ").collect();
-        
+
         if tokenized_line.len() == 1 && tokenized_line.contains(&"") {
             passports.push(temp_pass.clone());
             temp_pass = Passport::new();
@@ -106,15 +171,12 @@ pub fn run_part_1() -> Result<usize, String> {
 
         for item in tokenized_line {
             let (key, val) = {
-                let mut temp_pair= item.split(":");
-                
+                let mut temp_pair = item.split(":");
+
                 let temp_key = temp_pair.next();
                 let temp_val = temp_pair.next();
 
-                (
-                    temp_key.unwrap().to_string(),
-                    temp_val.unwrap().to_string(),
-                )
+                (temp_key.unwrap().to_string(), temp_val.unwrap().to_string())
             };
 
             temp_pass.insert(key, val);
@@ -124,11 +186,10 @@ pub fn run_part_1() -> Result<usize, String> {
 
     let mut count = 0;
 
-    for pass in passports{
-        if is_valid(&pass){
+    for pass in passports {
+        if is_valid(&pass) {
             count += 1;
         }
-
     }
 
     return Ok(count);
