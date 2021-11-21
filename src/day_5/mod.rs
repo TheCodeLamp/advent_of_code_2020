a4d597711616b4f24e37d093e70fa625f45dabbe
Simon Pertoft <simon@spectral.ninja>
2021-11-21 17:15:48 +0100

    Part 1 finished

--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -4,16 +4,16 @@ use crate::utils;
 fn translate_seat(pass: String) -> (usize, usize){
     assert!(pass.len() == 10);
 
-    let mut row_range: Vec<usize> = (0..127).collect();
-    let mut columns_range: Vec<usize> = (0..7).collect();
+    let mut row_range: Vec<usize> = (0..128).collect();
+    let mut columns_range: Vec<usize> = (0..8).collect();
 
     for char in pass.chars(){
         
         let row_middle = row_range.len()/2;
 
-        if char == 'F' {
+        if char == 'B' {
             row_range = row_range.split_off(row_middle);
-        } else if char == 'B' {
+        } else if char == 'F' {
             let _ = row_range.split_off(row_middle);
         }
 
@@ -24,12 +24,13 @@ fn translate_seat(pass: String) -> (usize, usize){
         } else if char == 'L' {
             let _ = columns_range.split_off(columns_middle);
         }
+
     }
 
-    assert_eq!(row_range.len(), 1);
-    assert_eq!(columns_range.len(), 1);
+    assert_eq!(row_range.len(), 1, "Row fail");
+    assert_eq!(columns_range.len(), 1, "Column fail");
 
-    return (0,0);
+    return (row_range[0],columns_range[0]);
 
 }
 
@@ -37,7 +38,18 @@ fn translate_seat(pass: String) -> (usize, usize){
 pub fn run_part_1() -> Result<usize, String>{
     let raw_input = utils::read_input("src/day_5/input").unwrap();
 
-    
+    let mut biggest = 0;
 
-    return Err("Not finished".to_string())
+    for line in raw_input{
+        let (row, column) = translate_seat(line);
+
+        let seat_id = row*8 + column;
+
+        if seat_id > biggest {
+            biggest = seat_id;
+        }
+
+    }
+
+    return Ok(biggest);
 }
